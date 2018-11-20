380bd2cb74338399d3401ecddff08bb15076f698
pengcheng <pengcheng3211@gmail.com>
2018-11-20 16:13:38 +0800

    增加spring自定义配置schemas的demo

---
+++ analysis-schema/src/main/java/com/analysis/schema/MyNamespaceHandler.java
@@ -0,0 +1,9 @@
+package com.analysis.schema;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+	public void init() {
+		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
+	}
+}
