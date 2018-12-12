f8aad3983f2dc64cc11ff712f0f7e10b3d6c9946
pengcheng <pengcheng3211@gmail.com>
2018-12-12 13:55:33 +0800

    style(): 格式化代码

--- analysis-schema/src/main/java/com/analysis/schema/MyNamespaceHandler.java
+++ analysis-schema/src/main/java/com/analysis/schema/MyNamespaceHandler.java
@@ -3,7 +3,9 @@ package com.analysis.schema;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 public class MyNamespaceHandler extends NamespaceHandlerSupport {
+
 	public void init() {
 		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
 	}
+
 }
